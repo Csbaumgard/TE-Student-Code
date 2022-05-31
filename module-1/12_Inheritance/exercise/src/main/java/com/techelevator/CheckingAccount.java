6d65116d91737621fcec368f682b3c2c4397813a
Colton Baumgard <csbaumgard@gmail.com>
2022-05-31 15:53:56 -0400

    finished

---
+++ module-1/12_Inheritance/exercise/src/main/java/com/techelevator/CheckingAccount.java
@@ -0,0 +1,26 @@
+package com.techelevator;
+
+public class CheckingAccount extends BankAccount {
+
+    public CheckingAccount(String accountHolderName, String accountNumber, int balance) {
+        super(accountHolderName, accountNumber, balance);
+    }
+
+    public CheckingAccount(String accountHolderName, String accountNumber) {
+        super(accountHolderName, accountNumber);
+    }
+
+    @Override
+    public int withdraw(int amountToWithdraw) {
+        if (getBalance() - amountToWithdraw <= -100) {
+            return getBalance();
+        }
+        super.withdraw(amountToWithdraw);
+        if (getBalance() < 0) {
+            super.withdraw(10);
+            return getBalance();
+        }
+        return getBalance();
+    }
+}
+
