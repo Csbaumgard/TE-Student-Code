ecaeb49dd1e525de29b0f7b976e1a527a699ffa3
Colton Baumgard <csbaumgard@gmail.com>
2022-06-06 14:21:24 -0400

    commit

--- module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
+++ module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -1,9 +1,54 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class WordSearch {
 
 	public static void main(String[] args) {
+		printApplicationBanner();
 
+		File inputFile = getInputFileFromUser();
+
+		Scanner wordScanner = new Scanner(System.in);
+		System.out.println("Please enter word to search for -> ");
+		String word = wordScanner.nextLine();
+
+		try (Scanner fileScanner = new Scanner(inputFile)) {
+			int lineCount = 0;
+			while (fileScanner.hasNextLine()) {
+				lineCount++;
+				String line = fileScanner.nextLine();
+				if (line.contains(word)) {
+					System.out.println(lineCount + ") " + line);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("The File Could Not Be Found");
+		}
+	}
+
+	private static File getInputFileFromUser() {
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Please enter path to input file -> ");
+		String path = userInput.nextLine();
+
+		File inputFile = new File(path);
+		if (!inputFile.exists()) {
+			System.out.println(path + " does not exist");
+			System.exit(1);
+		} else if (!inputFile.isFile()) {
+			System.out.println(path + " is not a file.");
+			System.exit(1);
+		}
+		return inputFile;
+	}
+
+	public static void printApplicationBanner() {
+		System.out.println("*******************");
+		System.out.println("****Word Search****");
+		System.out.println("*******************");
 	}
 
 }
--- module-1/16b_File_IO_Reading/lecture/src/main/java/com/techelevator/exceptions/ExceptionsLecture.java
+++ module-1/16b_File_IO_Reading/lecture/src/main/java/com/techelevator/exceptions/ExceptionsLecture.java
@@ -169,6 +169,7 @@ public class ExceptionsLecture {
 		return newBalance;		
 	}
 
+
 	private static void doSomethingDangerous() {
 		int[] numbers = new int[5];
 		for(int i = 0; i < 10; i++) {
