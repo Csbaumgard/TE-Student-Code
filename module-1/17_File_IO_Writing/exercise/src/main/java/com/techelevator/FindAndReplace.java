e71c3b8f895eecf49f8a468b2dff4478eb07daad
Colton Baumgard <csbaumgard@gmail.com>
2022-06-08 10:01:11 -0400

    Commit

--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,9 +1,63 @@
 package com.techelevator;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 public class FindAndReplace {
 
     public static void main(String[] args) {
+        printApplicationBanner();
 
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("What is the search word?");
+        String searchWord = userInput.nextLine();
+
+        System.out.println("What is the replacement word?");
+        String replacementWord = userInput.nextLine();
+
+        System.out.println("What is the source file?");
+        String sourcePath = userInput.nextLine();
+        File inputFile = new File(sourcePath);
+        if (!inputFile.exists()) {
+            System.out.println(sourcePath + " does not exist");
+            System.exit(1);
+        } else if (!inputFile.isFile()) {
+            System.out.println(sourcePath + " is not a file.");
+            System.exit(1);
+        }
+
+        System.out.println("What is the destination file?");
+        String destinationPath = userInput.nextLine();
+        File outputFile = new File(destinationPath);
+        if (!outputFile.exists()) {
+            System.out.println(destinationPath + " does not exist");
+            System.exit(1);
+        } else if (!outputFile.isFile()) {
+            System.out.println(destinationPath + " is not a file.");
+            System.exit(1);
+        }
+
+        try (PrintWriter writer = new PrintWriter(new FileOutputStream(destinationPath))) {
+            Scanner fileScanner = new Scanner(inputFile);
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                        if (line.contains(searchWord)) {
+                            writer.println(line.replaceAll(searchWord, replacementWord));
+                        } else {
+                            writer.println(line);
+                        }
+                    }
+                    System.out.println(outputFile);
+                } catch (FileNotFoundException e) {
+                    System.out.println("The file could not be found.");
+                    e.printStackTrace();
+                }
+        }
+        public static void printApplicationBanner () {
+            System.out.println("********************");
+            System.out.println("****Find&Replace****");
+            System.out.println("********************");
+        }
     }
-
-}
--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,9 +1,52 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class FizzWriter {
 
 	public static void main(String[] args) {
+		printApplicationBanner();
 
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Enter the destination file.");
+		String path = userInput.nextLine();
+		File outputFile = new File(path);
+
+
+		try (PrintWriter writer = new PrintWriter(new FileOutputStream(path))) {
+			int counter = 1;
+			while (counter <= 300) {
+				if (counter % 5 == 0 && counter % 3 == 0) {
+					writer.println("FizzBuzz");
+					counter++;
+				}
+				if (counter % 5 == 0) {
+					writer.println("Buzz");
+					counter++;
+				}
+				if (counter % 3 == 0) {
+					writer.println("Fizz");
+					counter++;
+				} else if (counter <= 300) {
+					writer.println(counter);
+					counter++;
+				}
+
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
+		public static void printApplicationBanner () {
+		System.out.println("*******************");
+		System.out.println("****Fizz_Writer****");
+		System.out.println("*******************");
+	}
+
 
 }
