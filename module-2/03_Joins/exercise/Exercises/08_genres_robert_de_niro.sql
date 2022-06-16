3beea49d16897efa8fad619d14a155102f710ca1
Colton Baumgard <csbaumgard@gmail.com>
2022-06-16 11:58:14 -0400

    commit

--- module-2/03_Joins/exercise/Exercises/08_genres_robert_de_niro.sql
+++ module-2/03_Joins/exercise/Exercises/08_genres_robert_de_niro.sql
@@ -1,10 +1,10 @@
 -- 8. The genres of movies that Robert De Niro has appeared in that were released in 2010 or later (6 rows)
 SELECT DISTINCT genre_name
 FROM movie
-JOIN movie_genre USING (movie_id)
-JOIN genre USING (genre_id)
-JOIN movie_actor USING (movie_id)
-JOIN person ON actor_id = person_id
+INNER JOIN movie_genre USING (movie_id)
+INNER JOIN genre USING (genre_id)
+INNER JOIN movie_actor USING (movie_id)
+INNER JOIN person ON actor_id = person_id
 WHERE person_name = 'Robert De Niro' AND release_date BETWEEN '1/1/2010' AND '6/15/2022'
 ORDER BY genre_name DESC;
 
