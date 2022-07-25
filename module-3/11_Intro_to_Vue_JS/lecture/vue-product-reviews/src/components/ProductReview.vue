fbd95fcf84b65a51c1698281231ea7161c822263
Colton Baumgard <csbaumgard@gmail.com>
2022-07-25 12:17:40 -0400

    commit

---
+++ module-3/11_Intro_to_Vue_JS/lecture/vue-product-reviews/src/components/ProductReview.vue
@@ -0,0 +1,41 @@
+<template>
+  <main>
+      <h2>Product Reviews for {{ name }}</h2>
+      <p class="description">{{ description }}</p>
+
+      <div class="review">
+          <h4></h4>
+          <div class="rating"></div>
+          <h3></h3>
+          <p></p>
+      </div>
+  </main>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            name: 'Cigar Parties for Dummies',
+            description: 'Host and plan the perfect cigar party for all your squirrelly friends.',
+            reviews: [
+                {
+                    id: 1,
+                    title: 'What a book!',
+                    reviewer: 'Malcolm Gladwell',
+                    rating: 3,
+                    review: 'It certainly is a book. I mean, I can see that.'
+                }
+            ]
+        }
+    }
+};
+</script>
+
+<style scoped>
+
+main.main {
+    margin: 1rem 0;
+}
+
+</style>
